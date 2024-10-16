3b5b27541e35dea332f7a0973c9e4a79de33770d
spcx86 <123968951+spcx86@users.noreply.github.com>
2024-10-16 12:19:45 +0530

    remove unrequired posts

--- app/about/opengraph-image.tsx
+++ app/about/opengraph-image.tsx
@@ -34,7 +34,6 @@ export default async function AboutOG() {
   ).then(res => res.arrayBuffer());
 
   const posts = await getPosts();
-  const viewsSum = posts.reduce((sum, post) => sum + post.views, 0);
 
   return new ImageResponse(
     (
@@ -78,7 +77,7 @@ export default async function AboutOG() {
           tw="flex w-full justify-center text-2xl text-gray-500"
           style={font("Roboto Mono 400")}
         >
-          {posts.length} posts / {commaNumber(viewsSum)} views
+          {posts.length} posts /
         </footer>
       </div>
     ),
