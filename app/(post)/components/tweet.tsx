7f29666e507c145934e55f14126eebae6a07565e
spcx86 <123968951+spcx86@users.noreply.github.com>
2024-10-16 11:39:02 +0530

    comment out

--- app/(post)/components/tweet.tsx
+++ app/(post)/components/tweet.tsx
@@ -1,58 +1,57 @@
-import { type ReactNode, Suspense } from "react";
-import { Tweet, getTweet } from "react-tweet/api";
-import {
-  EmbeddedTweet,
-  TweetNotFound,
-  TweetSkeleton,
-  type TweetProps,
-} from "react-tweet";
-import redis from "@/app/redis";
-import { Caption } from "./caption";
-import "./tweet.css";
+// import { type ReactNode, Suspense } from "react";
+// import { getTweet } from "react-tweet/api";
+// import {
+//   EmbeddedTweet,
+//   TweetNotFound,
+//   TweetSkeleton,
+//   type TweetProps,
+// } from "react-tweet"
+// import { Caption } from "./caption";
+// import "./tweet.css";
 
-interface TweetArgs {
-  id: string;
-  caption: ReactNode;
-}
+// interface TweetArgs {
+//   id: string;
+//   caption: ReactNode;
+// }
 
-async function getAndCacheTweet(id: string): Promise<Tweet | undefined> {
-  try {
-    const tweet = await getTweet(id);
-    // @ts-ignore
-    if (tweet && !tweet.tombstone) {
-      return tweet;
-    }
-  } catch (error) {
-    console.error("tweet fetch error", error);
-  }
+// async function getAndCacheTweet(id: string): Promise<Tweet | undefined> {
+//   try {
+//     const tweet = await getTweet(id);
+//     // @ts-ignore
+//     if (tweet && !tweet.tombstone) {
+//       return tweet;
+//     }
+//   } catch (error) {
+//     console.error("tweet fetch error", error);
+//   }
 
-  return undefined;
-}
+//   return undefined;
+// }
 
-const TweetContent = async ({ id, components }: TweetProps) => {
-  const tweet = id ? await getAndCacheTweet(id) : undefined;
+// const TweetContent = async ({ id, components }: TweetProps) => {
+//   const tweet = id ? await getAndCacheTweet(id) : undefined;
 
-  if (!tweet) {
-    return <TweetNotFound />;
-  }
+//   if (!tweet) {
+//     return <TweetNotFound />;
+//   }
 
-  return <EmbeddedTweet tweet={tweet} components={components} />;
-};
+//   return <EmbeddedTweet tweet={tweet} components={components} />;
+// };
 
-export const ReactTweet = (props: TweetProps) => (
-  <Suspense fallback={<TweetSkeleton />}>
-    {/* @ts-ignore: Async components are valid in the app directory */}
-    <TweetContent {...props} />
-  </Suspense>
-);
+// export const ReactTweet = (props: TweetProps) => (
+//   <Suspense fallback={<TweetSkeleton />}>
+//     {/* @ts-ignore: Async components are valid in the app directory */}
+//     <TweetContent {...props} />
+//   </Suspense>
+// );
 
-export async function Tweet({ id, caption }: TweetArgs) {
-  return (
-    <div className="tweet my-6">
-      <div className={`flex justify-center`}>
-        <ReactTweet id={id} />
-      </div>
-      {caption && <Caption>{caption}</Caption>}
-    </div>
-  );
-}
+// export async function Tweet({ id, caption }: TweetArgs) {
+//   return (
+//     <div className="tweet my-6">
+//       <div className={`flex justify-center`}>
+//         <ReactTweet id={id} />
+//       </div>
+//       {caption && <Caption>{caption}</Caption>}
+//     </div>
+//   );
+// }
